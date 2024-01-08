7a1832b37e4013fd4668e98e89cd65ed08e12cdf
Wasidy <vs.mayafoto@gmail.com>
2024-01-09 02:17:37 +0500

    Change tkinter to pygame engine

--- life_game.py
+++ life_game.py
@@ -5,95 +5,62 @@ Created on Mon Jan  8 17:23:06 2024
 @author: Wasidy
 """
 
+import builtins
+import sys
 import numpy as np
-#import matplotlib as mlib
-import matplotlib.pyplot as plt
-import time
-from PIL import Image, ImageTk
-
-import tkinter as tk
-
-x_size = 512
-y_size = 256
-
-view_scale = 4
+import pygame
 
 
+class LifeGame:
+    """Class of Conway's Game of Life"""
+    def __init__(self, x_size = 64, y_size = 64, view_scale = 10):
+        self.x_size = x_size
+        self.y_size = y_size
+        self.view_scale = view_scale
+        self.world = np.random.randint(2,size =(y_size,x_size))
+        self.world_ng = np.zeros((y_size,x_size), dtype=np.int16)
 
-root = tk.Tk()
+    def reset(self):
+        self.world = np.random.randint(2,size =(self.y_size,self.x_size))
 
-canvas = tk.Canvas(root,width=x_size*view_scale, height=y_size*view_scale)
-canvas.pack()
+    def step(self):
+        for active_y in range(self.y_size):
+            for active_x in range(self.x_size):
 
-#w = tk.Canvas(root, width = 900, height = 900)
-#w.pack()
+                inhabited = builtins.sum(self.world[(y+active_y-1) % self.y_size,(x+active_x-1) % self.x_size] \
+                    for x in range(3) for y in range(3) if not (y==1 and x==1))
 
+                self.world_ng[active_y, active_x] = 1 if (inhabited==3 and self.world[active_y, active_x]==0) \
+                    or (inhabited in range(2,4) and self.world[active_y, active_x] == 1) else 0
 
-# Define life grid
+        self.world = self.world_ng.copy()
 
-
-world = np.random.randint(2,size =(y_size,x_size))
-
-world_ng = np.zeros((y_size,x_size), dtype=np.int16)
-
-
-
-
-while True:
-#def life_game(world, world_ng, x_size, y_size, view_scale):
-    
-    canvas.delete("all")
-    for active_y in range(y_size):
-        for active_x in range(x_size):
-            inhabited = 0
-            cell_status = world[active_y, active_x]
-            
-            for y in range(-1,2,1):
-                for x in range(-1,2,1):
-                    if y==0 and x==0:
-                        
-                        continue
-                    n_y = (active_y + y) % y_size
-                    n_x = (active_x + x) % x_size
-                    inhabited += world[n_y, n_x]
-            
-            
-            if cell_status == 0:
-                if inhabited ==3:
-                    world_ng[active_y, active_x] = 1 #New life is born!
-
-            if cell_status == 1:
-            
-                if 2<= inhabited <=3:
-                    world_ng[active_y, active_x] = 1 #Lives
-                else:
-                    world_ng[active_y, active_x] = 0 #Dies
+        world_show = self.world_ng[:,:,np.newaxis]
+        world_show = world_show.repeat(self.view_scale, axis=0).repeat(self.view_scale, axis=1).repeat(3, axis=2)*255
+        return np.uint8(world_show).tobytes()
 
 
-    world_show = world_ng[:,:,np.newaxis]
-    world_show = world_show.repeat(view_scale, axis=0).repeat(view_scale, axis = 1).repeat(3, axis = 2)*255
+if __name__ == '__main__':
 
-    img = ImageTk.PhotoImage(image=Image.fromarray(np.uint8(world_show)))
+    x_s = 256
+    y_s = 128
+    v_scale = 5
+    my_life = LifeGame(x_size = x_s, y_size = y_s, view_scale = v_scale)
 
-    world = world_ng.copy()   
+    pygame.init()
+    screen = pygame.display.set_mode((x_s*v_scale,y_s*v_scale), vsync=1)
+    clock = pygame.time.Clock()
 
-    canvas.create_image(0,0, anchor="nw", image=img)
-    canvas.update()
-    #root.after(10, life_game(world, world_ng, x_size, y_size, view_scale))
-    #time.sleep(0.2)
+    while True:
+        clock.tick(10)
+        im2 = my_life.step()
+        pygame_surface = pygame.image.fromstring(im2, (x_s*v_scale,y_s*v_scale), "RGB")
+        screen.blit(pygame_surface, (0, 0))
+        pygame.display.flip()
+        for event in pygame.event.get():
 
-#root.mainloop()
-    #w.update()
-    #time.sleep(0.2)
-
-    #plt.pause(0.2)
-
-#life_game(world, world_ng, x_size, y_size, view_scale)
-print ("fuck")
-#root.mainloop()
-
-
-
-#for x in range(min, min+max):
-#    print ((x-1)%max)
-    
+            if event.type == pygame.KEYDOWN:
+                my_life.reset()
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
